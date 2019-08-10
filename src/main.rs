87cd9414211e7eaa26cd21188b79c83e7882ae7f
Denis Bazhenov <bazhenov@farpost.com>
2019-08-10 18:50:58 +1000

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use clap::App;
 mod processing;
 
 fn main() {
-	println!("Hello, world!");
+    println!("Hello, world!");
 }
 
 fn app<'a, 'b>() -> App<'a, 'b> {
-	App::new("sq")
-		.author("Denis Bazhenov")
-		.version("1.0")
-		.about("sequence processing toolchain")
-		.arg_from_usage("-f, --file=<FILE> 'Sets the input file'")
+    App::new("sq")
+        .author("Denis Bazhenov")
+        .version("1.0")
+        .about("sequence processing toolchain")
+        .arg_from_usage("-f, --file=<FILE> 'Sets the input file'")
 }
--- src/processing.rs
+++ src/processing.rs
@@ -1,33 +1,39 @@
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::ops::Range;
-use serde_json::{json};
-use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct Record {
-	text: String,
-	spans: Vec<Range<usize>>
+    text: String,
+    spans: Vec<Range<usize>>,
 }
 
 mod tests {
 
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn serialize() {
-		let r = Record { text: String::from("Hello"), spans: vec![] };
-		let expected = r#"{"text":"Hello","spans":[]}"#;
-		let value = serde_json::to_string(&r).unwrap();
-		assert_eq!(value, expected);
-	}
+    #[test]
+    fn serialize() {
+        let r = Record {
+            text: String::from("Hello"),
+            spans: vec![],
+        };
+        let expected = r#"{"text":"Hello","spans":[]}"#;
+        let value = serde_json::to_string(&r).unwrap();
+        assert_eq!(value, expected);
+    }
 
-	#[test]
-	fn deserialize() {
-		let expected = Record { text: String::from("Русский текст"), spans: vec![(13..15)] };
-		let json = json!({
-			"text": "Русский текст",
-			"spans": [[13, 15]]
-		});
-		let value: Record = serde_json::from_value(json).unwrap();
-		assert_eq!(value, expected)
-	}
+    #[test]
+    fn deserialize() {
+        let expected = Record {
+            text: String::from("Русский текст"),
+            spans: vec![(13..15)],
+        };
+        let json = json!({
+            "text": "Русский текст",
+            "spans": [[13, 15]]
+        });
+        let value: Record = serde_json::from_value(json).unwrap();
+        assert_eq!(value, expected)
+    }
 }
