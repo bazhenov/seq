cf1106fbd2e87d6fbca27068f569b849f7b30338
Denis Bazhenov <bazhenov@farpost.com>
2019-08-13 19:51:33 +1000

    Record::add_match() implementedc

--- src/processing.rs
+++ src/processing.rs
@@ -1,11 +1,35 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::io::{BufRead, BufReader, Read};
 use std::ops::Range;
 
+type Span = Range<usize>;
+
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Record {
     pub text: String,
-    pub spans: Vec<Range<usize>>,
+    pub spans: Vec<Span>,
+}
+
+impl Record {
+    fn new(text: &str) -> Self {
+        Record {
+            text: String::from(text),
+            spans: vec![],
+        }
+    }
+
+    fn add_match(&mut self, re: Regex) -> usize {
+        let mut found_spans = re
+            .find_iter(&self.text)
+            .map(|m| char_span(&self.text, m.start()..m.end()))
+            .collect::<Vec<_>>();
+
+        let spans_found = found_spans.len();
+        self.spans.append(&mut found_spans);
+
+        spans_found
+    }
 }
 
 pub struct Records<R> {
@@ -34,6 +58,20 @@ impl<R: Read> Iterator for Records<R> {
             },
         }
     }
+}
+
+/// Returns character span for a byte span and given string
+///
+/// This is convinient method for converting byte span (for example from a regex search) to a charatcter
+/// span for a given string.
+/// Input span should be correct byte offsets for a given string
+fn char_span(string: &str, span: Span) -> Span {
+    let (start, end) = (span.start, span.end);
+
+    let char_start = string[0..start].chars().count();
+    let char_end = char_start + string[start..end].chars().count();
+
+    char_start..char_end
 }
 
 mod tests {
@@ -73,5 +111,14 @@ mod tests {
 
         assert_eq!(records.next().map(|i| i.text), Some(String::from("Hello")));
         assert_eq!(records.next(), None);
+    }
+
+    #[test]
+    fn mark_records() {
+        let mut r = Record::new("Вот тебе 2 яблочка");
+        let regex = Regex::new("[0-9]+").unwrap();
+
+        assert_eq!(r.add_match(regex), 1);
+        assert_eq!(r.spans[0], 9..10);
     }
 }
